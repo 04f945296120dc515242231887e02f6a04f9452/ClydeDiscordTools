52a9c065fc2f684b33e0f76d68c21fed03cd7e4d
Tripl3S1x <120041039+Tripl3S1x@users.noreply.github.com>
2023-06-05 16:42:49 +0300

    Create answer.py

---
+++ answer.py
@@ -0,0 +1,7 @@
+class Answer:
+    """
+    A class representing an answer to a question.
+    """
+    def __init__(self, text, confidence):
+        self.text = text
+        self.confidence = confidence
